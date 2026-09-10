b6b4e6a952119cd48df5bb46c03d301e5c8849a9
hisse16 <hilmiselimsen@gmail.com>
2026-09-10 12:19:42 +0300

    Fix office styling import

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
+import './styles/office.css';
 import './services/story/dataIntegrity';
 import { cleanupLegacyMapArtifacts } from './services/cleanup/legacyMapCleanup';
 

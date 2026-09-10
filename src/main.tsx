a35dbe375a47b3e3826edb1a9210c54fd0c4ac60
hisse16 <hilmiselimsen@gmail.com>
2026-09-10 09:47:48 +0300

    Remove legacy map cleanup runtime dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './services/story/dataIntegrity';
-import { cleanupLegacyMapArtifacts } from './services/cleanup/legacyMapCleanup';
-
-cleanupLegacyMapArtifacts();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

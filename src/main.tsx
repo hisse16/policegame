4c5cb7e3e364a71f3d70341ffbb79aa5b97bf251
hisse16 <hilmiselimsen@gmail.com>
2026-09-10 10:30:50 +0300

    Fix App import after tutorial refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { App } from './App.tsx';
 import './index.css';
 import './services/story/dataIntegrity';
 import { cleanupLegacyMapArtifacts } from './services/cleanup/legacyMapCleanup';

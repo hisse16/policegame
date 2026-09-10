f7ffa4cbfc30969eed90eaa46dcd7e0948072fe2
hisse16 <hilmiselimsen@gmail.com>
2026-09-10 09:48:39 +0300

    Run legacy GIS cleanup safely before app boot

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './services/story/dataIntegrity';
+import { cleanupLegacyMapArtifacts } from './services/cleanup/legacyMapCleanup';
+
+cleanupLegacyMapArtifacts();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

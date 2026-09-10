a313bdf0e77a566be60de46edcac4ec675e56de1
hisse16 <hilmiselimsen@gmail.com>
2026-09-10 13:03:57 +0300

    Remove legacy workstation shell from app entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
 import React from 'react';
-import { GameProvider } from './context/GameContext';
 import { DetectiveGame } from './components/detective/DetectiveGame';
 
-/**
- * Detective Game 2.0
- *
- * The old workstation/police-computer layer is deliberately no longer part
- * of the player-facing game. The player is a private detective running a
- * newly opened agency.
- */
-export const App: React.FC = () => (
-  <GameProvider>
-    <DetectiveGame />
-  </GameProvider>
-);
+export const App: React.FC = () => <DetectiveGame />;
